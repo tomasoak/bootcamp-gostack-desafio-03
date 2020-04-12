c9738088bda1b8715c4a9c040496831784a99425
Tomás <tomas.oak23@gmail.com>
2020-04-12 09:50:29 -0300

    add config mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e8115b61448854',
+    pass: 'd4998b36e39aa6'
+  },
+  dafult: {
+    from: 'Equipe FastFeet <noreplay@fastfeet.com>',
+  }
+}
