e339658012f37714143d7c08990377c70cf80141
Tomás <tomas.oak23@gmail.com>
2020-04-12 17:16:29 -0300

    add package controller route

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 import DeliveryController from './app/controllers/DeliveryController';
+import PackageController from './app/controllers/PackageController';
 
 import authMiddleware from './app/middlewares/auth';
 import adminMiddlleware from './app/middlewares/admin';
@@ -29,8 +30,11 @@ routes.delete('/users/:id', UserController.delete);
 routes.post('/files', upload.single('file'), FileController.store);
 
 // CRUD for Packages - for deliveryman
-
-
+routes.get('/deliveryman/:id/to-delivery', PackageController.index);
+routes.get('/deliveryman/:id/delivered', PackageController.show);
+// Update delivery, withdraw or finish
+routes.put('/deliveryman/:id/deliveries/:deliveryId', PackageController.update);
+//routes.put('/deliveryman/:id/deliveries/:deliveryId/finish', FinishController.update);
 
 // CRUD For Deliveryman - for admin
 routes.post('/deliveryman',adminMiddlleware, DeliverymanController.store);
